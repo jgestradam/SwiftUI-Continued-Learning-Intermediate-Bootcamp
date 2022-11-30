9267da3d1d87bf2ba7d714b9eb7ae21f79fef527
jgestradam <jgestradam@unal.edu.co>
2022-11-30 17:05:27 -0500

    Class#6

---
+++ SwiftUI Continued Learning Intermediate Bootcamp/Class#6/GeometryReaderBootcamp.swift
@@ -0,0 +1,49 @@
+//
+//  GeometryReaderBootcamp.swift
+//  SwiftUI Continued Learning Intermediate Bootcamp
+//
+//  Created by Joseph Estrada on 11/30/22.
+//
+
+import SwiftUI
+
+struct GeometryReaderBootcamp: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20) { index in
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 20)
+                            .rotation3DEffect(
+                                Angle(degrees: getPercentage(geo: geometry) * 40),
+                                axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                }
+            }
+        }
+//        GeometryReader { geometry in
+//            HStack(spacing: 0) {
+//                Rectangle()
+//                    .fill(Color.red)
+//                    .frame(width: geometry.size.width * 0.6666)
+//                Rectangle().fill(Color.blue)
+//            }
+//            .ignoresSafeArea()
+//        }
+    }
+    
+    func getPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).midX
+        return Double(1 - (currentX / maxDistance))
+    }
+}
+
+struct GeometryReaderBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryReaderBootcamp()
+    }
+}
