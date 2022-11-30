4e15cf40f2ec5d51aad1b36c06ed71aef0cdc3af
jgestradam <jgestradam@unal.edu.co>
2022-11-30 09:22:30 -0500

    Initial Commit

---
+++ SwiftUI Continued Learning Intermediate Bootcamp/SwiftUI_Continued_Learning_Intermediate_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Continued_Learning_Intermediate_BootcampApp.swift
+//  SwiftUI Continued Learning Intermediate Bootcamp
+//
+//  Created by Joseph Estrada on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Continued_Learning_Intermediate_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
