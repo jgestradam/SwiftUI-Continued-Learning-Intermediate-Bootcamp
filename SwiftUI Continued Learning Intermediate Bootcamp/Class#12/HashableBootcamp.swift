fa8121d6a40734e2a60fc24e88798aa1fb17738f
jgestradam <jgestradam@unal.edu.co>
2022-12-02 22:15:50 -0500

    Class#12

---
+++ SwiftUI Continued Learning Intermediate Bootcamp/Class#12/HashableBootcamp.swift
@@ -0,0 +1,45 @@
+//
+//  HashableBootcamp.swift
+//  SwiftUI Continued Learning Intermediate Bootcamp
+//
+//  Created by Joseph Estrada on 12/1/22.
+//
+
+import SwiftUI
+
+struct MyCustomModel: Hashable {
+    let title: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+    }
+}
+
+struct HashableBootcamp: View {
+    
+    let data: [MyCustomModel] = [
+        MyCustomModel(title: "One"),
+        MyCustomModel(title: "Two"),
+        MyCustomModel(title: "Three"),
+        MyCustomModel(title: "Four"),
+        MyCustomModel(title: "Five")
+    ]
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 40) {
+                
+                ForEach(data, id: \.self) { item in
+                    Text(item.hashValue.description)
+                        .font(.headline)
+                }
+            }
+        }
+    }
+}
+
+struct HashableBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        HashableBootcamp()
+    }
+}
